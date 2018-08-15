900eeaa67d9d9a6eee1f643e7569e64fb0709dd1
Marina Delita <delita@ukr.net>
2018-08-15 23:18:42 +0300

    Final code

--- src/HomeWork_02.java
+++ src/HomeWork_02.java
@@ -15,5 +15,23 @@ public class HomeWork_02 {
 
         System.out.print("Хобби: ");
         String hobby = scanner.next();
+        System.out.println();
+        // Option 1 (tabular)
+        System.out.println("Имя:       " + name + "\n" +
+                "Город:     " + city + "\n" +
+                "Возраст:   " + age + "\n" +
+                "Хобби:     " + hobby);
+
+        System.out.println();
+        // Option 2 (text)
+        System.out.println("Человек по имени " + name + " живет в городе " + city + "\n" +
+                "Этому человеку " + age + " лет и любит он заниматься " + hobby);
+
+        System.out.println();
+        //Option 3 (other)
+        System.out.println(name + " - Имя" + "\n" +
+                city + " - Город" + "\n" +
+                age + " - Возвраст" + "\n" +
+                hobby + " - Хобби");
         }
 }
