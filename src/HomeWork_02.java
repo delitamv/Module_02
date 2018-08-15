bf648c47389ce67c27c25b155c293daaa31a9004
Marina Delita <delita@ukr.net>
2018-08-15 22:36:15 +0300

    Added module-02

---
+++ src/HomeWork_02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HomeWork_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Имя: ");
+        String name = scanner.nextLine();
+            }
+}
