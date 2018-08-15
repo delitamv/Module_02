2e494b0ca97ccf37d6c6c5ff3358a4a5d62a609b
Marina Delita <delita@ukr.net>
2018-08-15 22:55:26 +0300

    Update module-02

--- src/HomeWork_02.java
+++ src/HomeWork_02.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class HomeWork_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Имя: ");
         String name = scanner.nextLine();
-            }
+        System.out.println("Город: ");
+        String city = scanner.nextLine();
+                    }
 }
